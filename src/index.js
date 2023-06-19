e2ed9873fa139dca91da6c8154fd1e25e0641466
Ciro Miranda de Freitas <cirofreitas1995@gmail.com>
2023-06-18 21:21:47 -0300

    pokemon file handling and documentation

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import Pokedex from 'pokedex-promise-v2';
 import * as fs from 'fs';
+import curatePokemon from './pokemon.js';
 import capitalizeString from "./utils.js";
 
 // API Wrapper
@@ -8,77 +9,12 @@ const POKEDEX = new Pokedex();
 const REGION_DEX = "kanto";
 
 function start() {
-    curatePokemon().then((response) => {
-      const { abilities, moves } = response;
-      curateAbilities(abilities);
-      curateMoves(moves);
-    });
-}
-
-async function curatePokemon() {
-  console.log("Starting pokemon data collection!");
-  const curatedPokemons = [];
-  const abilities = [];
-  const moves = [];
-  
-  const searchDex = await POKEDEX.getPokedexByName(REGION_DEX);
-  for(const entry of searchDex.pokemon_entries) {
-    const pokemon = await POKEDEX.getPokemonByName(entry.pokemon_species.name);
-    const pokemonLatestGameVersion = await getPokemonLatestMoveSetVersion(pokemon.name, pokemon.moves);
-    const pokemonAbilities = [];
-    const pokemonMoves = [];
-
-    for(const move of pokemon.moves) {
-      // Checks if pokemons learns move in the desired game version and how
-      const moveGameVersion = move.version_group_details.find((version) => version.version_group.name == pokemonLatestGameVersion);
-      if(moveGameVersion) {
-        pokemonMoves.push({
-          name: move.move.name,
-          learningMethod: moveGameVersion.move_learn_method.name,
-        });
-
-        // Get relevant abilities names
-        const isMoveAlreadyCollected = moves.find((collectedMove) => collectedMove == move.move.name);
-        if(!isMoveAlreadyCollected) {
-          moves.push(move.move.name);
-        }
-      }
-    }
-
-    for(const ability of pokemon.abilities) {
-      pokemonAbilities.push(ability.ability.name)
-  
-      // Get relevant abilities names
-      const isAbilityAlreadyCollected = abilities.find((collectedAbility) => collectedAbility == ability.ability.name);
-      if(!isAbilityAlreadyCollected) {
-        abilities.push(ability.ability.name);
-      }
-    }
-  
-    // Pokemon data collector
-    curatedPokemons.push({
-      name: pokemon.name,
-      primaryType: pokemon.types[0].type.name,
-      secondaryType: pokemon.types.length > 1 ? pokemon.types[1].type.name : "",
-      abilities: abilities,
-      hitpoints: Math.max(Math.round(pokemon.stats[0].base_stat/10), 1),
-      attack: Math.max(Math.round(pokemon.stats[1].base_stat/20), 1),
-      defense: Math.max(Math.round(pokemon.stats[2].base_stat/20), 1),
-      specialAttack: Math.max(Math.round(pokemon.stats[3].base_stat/20), 1),
-      specialDefense: Math.max(Math.round(pokemon.stats[4].base_stat/20), 1),
-      speed: Math.max(Math.round(pokemon.stats[5].base_stat/20), 1),
-      moves: pokemonMoves,
-    });
-    console.log(firstLettertoUpperCase(pokemon.name) + " collected!")
-  }
-
-  // Write file with curated pokemons data and returns all relevant abilities and moves names
-  fs.writeFileSync("./collected_data/pokemons.json", JSON.stringify(curatedPokemons));
-  console.log("Pokemons " + curatedPokemons.length + " collected!");
-  return {
-    abilities: abilities,
-    moves: moves,
-  };
+  const pokedex = new Pokedex();
+  curatePokemon(pokedex, REGION_DEX).then((response) => {
+    const { abilities, moves } = response;
+    curateAbilities(abilities);
+    curateMoves(moves);
+  });
 }
 
 async function curateAbilities(abilities) {
@@ -126,11 +62,6 @@ async function curateMoves(moves) {
   console.log("Moves " + curatedMoves.length + " collected!");
 }
 
-// Utils
-function firstLettertoUpperCase(str) {
-  return str.charAt(0).toUpperCase() + str.slice(1);
-}
-
 function getMoveExhaustion(pp) {
   switch(true) {
     case pp < 10:
@@ -145,32 +76,6 @@ function getMoveExhaustion(pp) {
     default:
       return 0;
   }
-}
-
-async function getPokemonLatestMoveSetVersion(pokemonName, moves) {
-  for(const move of moves) {
-    switch(true) {
-      case move.version_group_details.find((version) => version.version_group.name == "scarlet-violet") != undefined:
-        return "scarlet-violet";
-  
-      case move.version_group_details.find((version) => version.version_group.name == "sword-shield") != undefined:
-        return "sword-shield";
-  
-      case move.version_group_details.find((version) => version.version_group.name == "brilliant-diamond-and-shining-pearl") != undefined:
-        return "brilliant-diamond-and-shining-pearl";
-  
-        case move.version_group_details.find((version) => version.version_group.name == "lets-go-pikachu-lets-go-eevee") != undefined:
-          return "lets-go-pikachu-lets-go-eevee";
-    }
-  }
-      
-  // Used to detect which pokemon is missing in order to added more cases above
-  //moves[0].version_group_details.forEach((version) => {
-  //  console.log(version);
-  //});
-  //console.log(await POKEDEX.getVersionGroupsList());
-  console.log("No game version with " + firstLettertoUpperCase(pokemonName) + " moves! :(");
-  process.exit();
 }
 
 async function getLatestFlavorText(flavor_text_entries, name) {  
---
+++ src/pokemon.js
@@ -0,0 +1,112 @@
+// Pokemon data gathering methods
+
+import * as fs from 'fs';
+import capitalizeString from "./utils.js";
+
+/**
+ * This will get all pokemon from the specified region's pokedex with latest possible game info.
+ * It's data is curated for my needs, so it may not make sense as is.
+ * Beware some regions with bigger pokedex entry, notably the national dex, will take it's sweet time curating.
+ * 
+ * Reponse: Object{ abilities: string[], moves: string[] }
+ * 
+ * @param {Pokedex} pokedex 
+ * @param {string} regionDex 
+ * @returns {Reponse}
+ */
+export default async function curatePokemon(pokedex, regionDex) {
+    console.log("Starting pokemon data collection!");
+    const curatedPokemons = [];
+    const abilities = [];
+    const moves = [];
+    
+    const searchDex = await pokedex.getPokedexByName(regionDex);
+    for(const entry of searchDex.pokemon_entries) {
+        const pokemon = await pokedex.getPokemonByName(entry.pokemon_species.name);
+        const pokemonLatestGameVersion = await getPokemonLatestMoveSetVersion(pokemon.name, pokemon.moves);
+        const pokemonAbilities = [];
+        const pokemonMoves = [];
+  
+        for(const move of pokemon.moves) {
+            // Checks if pokemons learns move in the desired game version and how
+            const moveGameVersion = move.version_group_details.find((version) => version.version_group.name == pokemonLatestGameVersion);
+            if(moveGameVersion) {
+                pokemonMoves.push({
+                    name: move.move.name,
+                    learningMethod: moveGameVersion.move_learn_method.name,
+                });
+  
+                // Get relevant moves names
+                const isMoveAlreadyCollected = moves.find((collectedMove) => collectedMove == move.move.name);
+                if(!isMoveAlreadyCollected) {
+                    moves.push(move.move.name);
+                }
+            }
+        }
+  
+        for(const ability of pokemon.abilities) {
+            pokemonAbilities.push(ability.ability.name)
+    
+            // Get relevant abilities names
+            const isAbilityAlreadyCollected = abilities.find((collectedAbility) => collectedAbility == ability.ability.name);
+            if(!isAbilityAlreadyCollected) {
+                abilities.push(ability.ability.name);
+            }
+        }
+    
+        // Pokemon data collector
+            curatedPokemons.push({
+            name: pokemon.name,
+            primaryType: pokemon.types[0].type.name,
+            secondaryType: pokemon.types.length > 1 ? pokemon.types[1].type.name : "",
+            abilities: abilities,
+            hitpoints: Math.max(Math.round(pokemon.stats[0].base_stat/10), 1),
+            attack: Math.max(Math.round(pokemon.stats[1].base_stat/20), 1),
+            defense: Math.max(Math.round(pokemon.stats[2].base_stat/20), 1),
+            specialAttack: Math.max(Math.round(pokemon.stats[3].base_stat/20), 1),
+            specialDefense: Math.max(Math.round(pokemon.stats[4].base_stat/20), 1),
+            speed: Math.max(Math.round(pokemon.stats[5].base_stat/20), 1),
+            moves: pokemonMoves,
+        });
+
+        console.log(capitalizeString(pokemon.name) + " collected!")
+    }
+  
+    // Write file with curated pokemons data and returns all relevant abilities and moves names
+    fs.writeFileSync("./collected_data/pokemons.json", JSON.stringify(curatedPokemons));
+    console.log("Pokemons " + curatedPokemons.length + " collected!");
+    return {
+      abilities: abilities,
+      moves: moves,
+    };
+}
+
+/**
+ * Tries to find which vertsion the requested pokémon move set is the latest, moves object expects to be a
+ * move endpoint from a pokemon using pokedex-promise-v2. If no version is found, process will shut down as
+ * it's intended to sinalize that I need to find the next possible version and add to the switch case.
+ * 
+ * @param {string} pokemonName 
+ * @param {object} moves 
+ * @returns string
+ */
+async function getPokemonLatestMoveSetVersion(pokemonName, moves) {
+    for(const move of moves) {
+        switch(true) {
+            case move.version_group_details.find((version) => version.version_group.name == "scarlet-violet") !=  undefined:
+                return "scarlet-violet";
+    
+            case move.version_group_details.find((version) => version.version_group.name == "sword-shield") != undefined:
+                return "sword-shield";
+    
+            case move.version_group_details.find((version) => version.version_group.name == "brilliant-diamond-and-shining-pearl") != undefined:
+                return "brilliant-diamond-and-shining-pearl";
+    
+            case move.version_group_details.find((version) => version.version_group.name == "lets-go-pikachu-lets-go-eevee") != undefined:
+                return "lets-go-pikachu-lets-go-eevee";
+        }
+    }
+    
+    console.log("No game version with " + capitalizeString(pokemonName) + " moves! :(");
+    process.exit();
+}
